676bd5389e4479b70c955f6e73fc10e5c618cc53
_Coder_Naren <154037829+CoderNaren00@users.noreply.github.com>
2024-02-15 13:20:31 +0530

    Add files via upload

---
+++ Web-Development(CodewithHarry)New/JS files/js video 54 introduction/Video 55( variable, data types & Objects)/script.js
@@ -0,0 +1,39 @@
+console.log("Hey this is tutorial 55 of javascript")
+
+let a = 5;
+a = a + 1;
+let b = 6;
+let c = "Narendra";
+console.log(a + b)
+console.log(c)
+console.log(typeof a, typeof b, typeof c, typeof cons);
+{
+    let a = 4;
+    console.log(a)
+}
+console.log(a);
+
+// const a1 = 7; not allowed
+// a1 = a1 + 1; Not allowed because a1 is constant
+
+let x = "harry bhai";
+let y = "22";
+let z = "3.14";
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    "name": "Harry",
+    "Job Code": 2435
+    "is_handsome": true
+}
+
+console.log(o)
+o.salary = "100 crores";
+console.log(o);
+o.salary = "500 crores";
+console.log(o);
