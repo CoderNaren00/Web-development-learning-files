0c8f604138658721258276bb1d95f9995e44159f
_Coder_Naren <154037829+CoderNaren00@users.noreply.github.com>
2024-02-16 10:54:56 +0530

    Add files via upload

---
+++ Web-Development(CodewithHarry)New/JS files/js video 54 introduction/Video 58(JavaScript Loop)/index.js
@@ -0,0 +1,37 @@
+console.log("I am a tutorial on loops");
+
+// let a = 1;
+// 
+// for (let i = 0; i < 100 ; i++) {
+    // console.log(a + 1);
+// 
+// }
+// 
+// let obj = {
+    // name: "Narendra",
+    // role : "Programmer",
+    // company : "CodeWithHarry AI"
+// }
+// 
+// for (const key in obj) {
+        // const element = obj[key];
+        // console.log(key)
+// }
+// 
+// for (const c of "Narendra") {
+    // console.log(c)
+// }
+// 
+
+// let i = 0;
+// while (i<700) {
+    // console.log(i)
+    // i++;    
+// }
+// 
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
