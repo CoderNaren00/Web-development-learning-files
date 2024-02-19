c45d960de1002a94f896965c0d67f604e52ea18d
_Coder_Naren <154037829+CoderNaren00@users.noreply.github.com>
2024-02-19 14:12:13 +0530

    Add files via upload

---
+++ Web-Development(CodewithHarry)New/JS files/js video 54 introduction/Video 61(Strings)/index.js
@@ -0,0 +1,29 @@
+console.log("This is string tutorial")
+let a = "Narendra";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+// console.log(a[8])
+console.log(a.length)
+
+let Name = "Narendra"
+let Friend = "Hanumant"
+
+console.log("His name is " + Name + " and his friend name is " + Friend)
+console.log(`His name is ${Name} and his friend name is ${Friend}`)//Template litrals//
+
+let b = "Shivam";
+console.log(b.toUpperCase())
+console.log(b.length)
+console.log(b.toLowerCase())
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+
+console.log(b.replace("am", "21"))
+console.log(b.concat(a,"Sakshi"))
+console.log(b)
